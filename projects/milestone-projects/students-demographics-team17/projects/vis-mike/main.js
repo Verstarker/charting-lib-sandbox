4f147d91ed882967fdd8b8086227ca37d7ab77a7
Michael Hopping <mhopping@xtra.co.nz>
2018-01-26 12:25:52 +1300

    added mike folder

---
+++ projects/milestone-projects/students-demographics-team17/projects/vis-mike/main.js
@@ -0,0 +1,47 @@
+Highcharts.chart('container-chart', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Which Area Of<br>Christchurch<br>Do You Live In?',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 40
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                distance: -50,
+                style: {
+                    fontWeight: 'bold',
+                    color: 'white'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%']
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Percentage of People',
+        innerSize: '50%',
+        data: [
+            ['North',   2],
+            ['South',       2],
+            ['West', 3],
+            ['Central',    1],
+            ['Other',     1],
+            {
+               
+            }
+        ]
+    }]
+});
+
